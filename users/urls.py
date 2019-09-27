0dde492f18e37d60bd92632d7e16fddaecb4ac39
Angela <50228063+angelamac999@users.noreply.github.com>
2019-09-27 21:22:45 +0800

    Fixed after help with database and admin issues

--- users/urls.py
+++ users/urls.py
@@ -3,11 +3,18 @@ from django.conf.urls import include, url
 from . import views
 
 
+urlpatterns = [path("register/", views.Register.as_view(), name="register")]
 
-app_name = 'users'
 
-urlpatterns = [
-    path('event-finder/', include('eventFinderApp.urls')),
-    path('register/', views.Register.as_view(), name='register'),
-    path('users/', include('django.contrib.auth.urls')),
-   ]
+# from django.urls import path, include
+# from django.conf.urls import include, url
+# from . import views
+
+
+
+# app_name = 'users'
+
+# urlpatterns = [
+#     path('register/', views.Register.as_view(), name='register'),
+# ]
+
